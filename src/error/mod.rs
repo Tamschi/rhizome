ae38aed4cb95b771fa11ef5d02a9acbe6d41ff75
Tamme Schichler <tamme@schichler.dev>
2020-11-14 22:52:09 +0100

    Fixed a few warnings

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -1,44 +1,76 @@
 use core::fmt::{Debug, Display, Error as fmtError, Formatter};
 use std::error::Error as stdError;
 
-pub mod auto_provision_error;
-
 #[derive(Debug)]
 pub enum Error {
-	NoDefault,
-	NoTagMatched,
-	Other(Box<dyn stdError>),
+    NoDefault,
+    NoTagMatched,
+    Other(Box<dyn stdError>),
 }
 
 impl stdError for Error {
-	fn source(&self) -> Option<&(dyn stdError + 'static)> {
-		use Error::*;
-		match self {
-			NoDefault => None,
-			NoTagMatched => None,
-			Other(source) => Some(source.as_ref()),
-		}
-	}
+    fn source(&self) -> Option<&(dyn stdError + 'static)> {
+        match self {
+            Error::NoDefault | Error::NoTagMatched => None,
+            Error::Other(source) => Some(source.as_ref()),
+        }
+    }
 }
 impl Display for Error {
-	fn fmt(&self, fmt: &mut Formatter<'_>) -> Result<(), fmtError> {
-		use Error::*;
-		match self {
-			NoDefault => writeln!(fmt, "Not provided.")?,
-			NoTagMatched => writeln!(fmt, "No matching tag found to default-provide at.")?,
-			Other(o) => write!(fmt, "{:#}", o)?,
-		}
-		Ok(())
-	}
+    fn fmt(&self, fmt: &mut Formatter<'_>) -> Result<(), fmtError> {
+        match self {
+            Error::NoDefault => writeln!(fmt, "Not provided.")?,
+            Error::NoTagMatched => writeln!(fmt, "No matching tag found to default-provide at.")?,
+            Error::Other(o) => write!(fmt, "{:#}", o)?,
+        }
+        Ok(())
+    }
 }
 
 impl PartialEq for Error {
-	fn eq(&self, rhs: &Error) -> bool {
-		use Error::*;
-		match (self, rhs) {
-			(NoDefault, NoDefault) => true,
-			(NoTagMatched, NoTagMatched) => true,
-			_ => false,
-		}
-	}
+    fn eq(&self, rhs: &Error) -> bool {
+        match (self, rhs) {
+            (Error::NoDefault, Error::NoDefault) | (Error::NoTagMatched, Error::NoTagMatched) => {
+                true
+            }
+            _ => false,
+        }
+    }
+}
+
+#[allow(clippy::module_name_repetitions)]
+#[derive(Debug)]
+pub enum AutoProvisionError {
+    KeyExists,
+    MissingDependency(&'static str, Error),
+}
+impl stdError for AutoProvisionError {
+    fn source(&self) -> Option<&(dyn stdError + 'static)> {
+        match self {
+            AutoProvisionError::KeyExists => None,
+            AutoProvisionError::MissingDependency(_, source) => Some(source),
+        }
+    }
+}
+
+impl Display for AutoProvisionError {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), fmtError> {
+        match self {
+            AutoProvisionError::KeyExists => writeln!(fmt, "Key exists."),
+            AutoProvisionError::MissingDependency(dependency, source) => match source {
+                Error::NoDefault => writeln!(fmt, "{} (No default provision.)", dependency),
+                Error::NoTagMatched => writeln!(
+                    fmt,
+                    "{} (Could not find matching tag to provide at.)",
+                    dependency
+                ),
+                Error::Other(_) => write!(
+                    fmt,
+                    "{}\n\
+						-> {:#}",
+                    dependency, source
+                ),
+            },
+        }
+    }
 }
