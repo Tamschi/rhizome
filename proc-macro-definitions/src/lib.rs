812a67ca226ab540e705cd2b42eee5b33bd6097f
Tamme Schichler <tamme@schichler.dev>
2020-11-15 02:43:46 +0100

    [WIP]

--- proc-macro-definitions/src/lib.rs
+++ proc-macro-definitions/src/lib.rs
@@ -7,28 +7,26 @@ extern crate proc_macro;
 
 mod extractable_declaration;
 
-use {
-    extractable_declaration::ExtractableDeclaration,
-    lazy_static::lazy_static,
-    proc_macro::TokenStream as TokenStream1,
-    proc_macro2::{Span, TokenStream as TokenStream2},
-    proc_macro_crate::crate_name,
-    syn::{parse_macro_input, Ident},
-};
+use extractable_declaration::ExtractableDeclaration;
+use lazy_static::lazy_static;
+use proc_macro::TokenStream as TokenStream1;
+use proc_macro2::{Span, TokenStream as TokenStream2};
+use proc_macro_crate::crate_name;
+use syn::{parse_macro_input, Ident};
 
 #[proc_macro]
 pub fn extractable(input: TokenStream1) -> TokenStream1 {
-    let extractable_declaration = parse_macro_input!(input as ExtractableDeclaration);
-    let tokens: TokenStream2 = extractable_declaration
-        .into_tokens()
-        .unwrap_or_else(|error| error.to_compile_error());
-    tokens.into()
+	let extractable_declaration = parse_macro_input!(input as ExtractableDeclaration);
+	let tokens: TokenStream2 = extractable_declaration
+		.into_tokens()
+		.unwrap_or_else(|error| error.to_compile_error());
+	tokens.into()
 }
 
 lazy_static! {
-    static ref RHIZOME_NAME: String =
-        crate_name("rhizome").unwrap_or_else(|_| "rhizome".to_owned());
+	static ref RHIZOME_NAME: String =
+		crate_name("rhizome").unwrap_or_else(|_| "rhizome".to_owned());
 }
 fn rhizome_ident(span: Span) -> Ident {
-    Ident::new(&*RHIZOME_NAME, span)
+	Ident::new(&*RHIZOME_NAME, span)
 }
--- src/error/mod.rs
+++ src/error/mod.rs
@@ -3,74 +3,74 @@ use std::error::Error as stdError;
 
 #[derive(Debug)]
 pub enum Error {
-    NoDefault,
-    NoTagMatched,
-    Other(Box<dyn stdError>),
+	NoDefault,
+	NoTagMatched,
+	Other(Box<dyn stdError>),
 }
 
 impl stdError for Error {
-    fn source(&self) -> Option<&(dyn stdError + 'static)> {
-        match self {
-            Error::NoDefault | Error::NoTagMatched => None,
-            Error::Other(source) => Some(source.as_ref()),
-        }
-    }
+	fn source(&self) -> Option<&(dyn stdError + 'static)> {
+		match self {
+			Error::NoDefault | Error::NoTagMatched => None,
+			Error::Other(source) => Some(source.as_ref()),
+		}
+	}
 }
 impl Display for Error {
-    fn fmt(&self, fmt: &mut Formatter<'_>) -> Result<(), fmtError> {
-        match self {
-            Error::NoDefault => writeln!(fmt, "Not provided.")?,
-            Error::NoTagMatched => writeln!(fmt, "No matching tag found to default-provide at.")?,
-            Error::Other(o) => write!(fmt, "{:#}", o)?,
-        }
-        Ok(())
-    }
+	fn fmt(&self, fmt: &mut Formatter<'_>) -> Result<(), fmtError> {
+		match self {
+			Error::NoDefault => writeln!(fmt, "Not provided.")?,
+			Error::NoTagMatched => writeln!(fmt, "No matching tag found to default-provide at.")?,
+			Error::Other(o) => write!(fmt, "{:#}", o)?,
+		}
+		Ok(())
+	}
 }
 
 impl PartialEq for Error {
-    fn eq(&self, rhs: &Error) -> bool {
-        match (self, rhs) {
-            (Error::NoDefault, Error::NoDefault) | (Error::NoTagMatched, Error::NoTagMatched) => {
-                true
-            }
-            _ => false,
-        }
-    }
+	fn eq(&self, rhs: &Error) -> bool {
+		match (self, rhs) {
+			(Error::NoDefault, Error::NoDefault) | (Error::NoTagMatched, Error::NoTagMatched) => {
+				true
+			}
+			_ => false,
+		}
+	}
 }
 
 #[allow(clippy::module_name_repetitions)]
 #[derive(Debug)]
 pub enum AutoProvisionError {
-    KeyExists,
-    MissingDependency(&'static str, Error),
+	KeyExists,
+	MissingDependency(&'static str, Error),
 }
 impl stdError for AutoProvisionError {
-    fn source(&self) -> Option<&(dyn stdError + 'static)> {
-        match self {
-            AutoProvisionError::KeyExists => None,
-            AutoProvisionError::MissingDependency(_, source) => Some(source),
-        }
-    }
+	fn source(&self) -> Option<&(dyn stdError + 'static)> {
+		match self {
+			AutoProvisionError::KeyExists => None,
+			AutoProvisionError::MissingDependency(_, source) => Some(source),
+		}
+	}
 }
 
 impl Display for AutoProvisionError {
-    fn fmt(&self, fmt: &mut Formatter) -> Result<(), fmtError> {
-        match self {
-            AutoProvisionError::KeyExists => writeln!(fmt, "Key exists."),
-            AutoProvisionError::MissingDependency(dependency, source) => match source {
-                Error::NoDefault => writeln!(fmt, "{} (No default provision.)", dependency),
-                Error::NoTagMatched => writeln!(
-                    fmt,
-                    "{} (Could not find matching tag to provide at.)",
-                    dependency
-                ),
-                Error::Other(_) => write!(
-                    fmt,
-                    "{}\n\
+	fn fmt(&self, fmt: &mut Formatter) -> Result<(), fmtError> {
+		match self {
+			AutoProvisionError::KeyExists => writeln!(fmt, "Key exists."),
+			AutoProvisionError::MissingDependency(dependency, source) => match source {
+				Error::NoDefault => writeln!(fmt, "{} (No default provision.)", dependency),
+				Error::NoTagMatched => writeln!(
+					fmt,
+					"{} (Could not find matching tag to provide at.)",
+					dependency
+				),
+				Error::Other(_) => write!(
+					fmt,
+					"{}\n\
 						-> {:#}",
-                    dependency, source
-                ),
-            },
-        }
-    }
+					dependency, source
+				),
+			},
+		}
+	}
 }
