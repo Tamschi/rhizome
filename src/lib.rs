de0374cbb860db664ca0d80723aadf372527bf66
Tamme Schichler <tamme@schichler.dev>
2021-09-07 10:34:24 +0200

    Initial template commit

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+#![doc(html_root_url = "https://docs.rs/TODO_CRATE_NAME/0.0.1")]
+#![warn(clippy::pedantic)]
+#![allow(clippy::semicolon_if_nothing_returned)]
+
+#[cfg(doctest)]
+pub mod readme {
+	doc_comment::doctest!("../README.md");
+}
